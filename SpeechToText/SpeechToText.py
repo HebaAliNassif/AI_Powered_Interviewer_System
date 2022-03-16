5e3d076cc49904249daed06b5f906c5123a2604c
aya1sameh <49209317+aya1sameh@users.noreply.github.com>
2022-03-16 23:47:28 +0200

    adding the speech to text module

---
+++ SpeechToText/SpeechToText.py
@@ -0,0 +1,82 @@
+#Reference:
+#https://www.thepythoncode.com/article/using-speech-recognition-to-convert-speech-to-text-python
+
+
+from distutils import extension
+import os
+import sys
+import speech_recognition as sr
+from moviepy.editor import VideoFileClip
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+
+# a function that splits the audio file into chunks and applies speech recognition
+def get_large_audio_transcription(path):
+    """
+    Splitting the large audio file into chunks
+    and apply speech recognition on each of these chunks
+    """
+    # create a speech recognition object
+    r = sr.Recognizer()
+    # open the audio file using pydub
+    sound = AudioSegment.from_wav(path)  
+    # split audio sound where silence is 700 miliseconds or more and get chunks
+    chunks = split_on_silence(sound,
+        # experiment with this value for your target audio file
+        min_silence_len = 500,
+        # adjust this per requirement
+        silence_thresh = sound.dBFS-14,
+        # keep the silence for 1 second, adjustable as well
+        keep_silence=500,
+    )
+    folder_name = "audio-chunks"
+    # create a directory to store the audio chunks
+    if not os.path.isdir(folder_name):
+        os.mkdir(folder_name)
+    whole_text = ""
+    # process each chunk 
+    for i, audio_chunk in enumerate(chunks, start=1):
+        # export audio chunk and save it in
+        # the `folder_name` directory.
+        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
+        audio_chunk.export(chunk_filename, format="wav")
+        # recognize the chunk
+        with sr.AudioFile(chunk_filename) as source:
+            audio_listened = r.record(source)
+            # try converting it to text
+            try:
+                text = r.recognize_google(audio_listened)
+            except sr.UnknownValueError as e:
+                text = "."
+                whole_text += text
+                #print("Error:", str(e))
+            else:
+                text = f"{text.capitalize()}. "
+                #print(chunk_filename, ":", text)
+                whole_text += text
+    # return the text for all chunks detected
+    return whole_text
+#a function that convert mp4 to wav
+def convert_video_to_audio_moviepy(video_file, output_ext="wav"):
+    """Converts video to audio using MoviePy library
+    that uses `ffmpeg` under the hood"""
+    filename, ext = os.path.splitext(video_file)
+    clip = VideoFileClip(video_file)
+    clip.audio.write_audiofile(f"{filename}.{output_ext}")
+
+
+file_path=input('Please enter the mp4 file path: ')
+filename, exten = os.path.splitext(file_path)
+while exten !=".mp4":
+    print(exten)
+    file_path=input('Please enter the mp4 file path correctly: ')
+    filename, exten = os.path.splitext(file_path)
+
+convert_video_to_audio_moviepy(file_path)
+path = filename+".wav"
+print("Converting speech to text....")
+text=get_large_audio_transcription(path)
+print("Conversion complete check the SpeechToTextOutput.txt file")
+f= open("SpeechToTextOutput.txt","w+")
+f.write(text)
+f.close()    
---
+++ SpeechToText/readme.txt
@@ -0,0 +1,4 @@
+#To run the module:
+-> Write in the cmd python SpeechToText.python
+-> Enter the input mp4 file name (i.e..numbers.mp4) 
+-> The output will be in the SpeechToTextOutput.txt file and the audio chunks in the audio-chunks folder
