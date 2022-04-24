1956acb76f7e68f1b4541691d28facc1a59bfbed
aya1sameh <49209317+aya1sameh@users.noreply.github.com>
2022-04-25 00:31:45 +0200

    removing the nav bar from the interface

--- Interface/frontend/src/App.vue
+++ Interface/frontend/src/App.vue
@@ -18,38 +18,9 @@
         </v-tabs>
       </template>
       <v-spacer></v-spacer>
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      
     </v-app-bar>
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-      right
-    >
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-          v-model="group"
-          active-class="blue--text text--accent-4"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Account</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
+   
     <v-main>
       <router-view/>
     </v-main>
